4f897ce01032b064c69b28b154f5c99223749c82
Your Name <you@example.com>
2020-03-18 15:25:59 +0530

    first commit

---
+++ js/config.js
@@ -0,0 +1,7 @@
+//var url = 'http://localhost:8000'
+//var api_url = 'http://localhost:5000'
+//var mode = 'sandbox'
+
+var url = 'https://tantra.me'
+var api_url = 'https://go.tantra.me'
+var mode = 'production'
